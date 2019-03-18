1eacae45536ad4a30c043500ec4d91381617e658
Julio <jalfaro@techlaunch.io>
2019-03-18 17:57:56 -0400

    update

---
+++ build/precache-manifest.8c27a2bb1d656c731d17697d4950d0e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2f700a36f697fcfcf937",
+    "url": "/static/js/main.6ff9e00d.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "2f700a36f697fcfcf937",
+    "url": "/static/css/main.65a31741.chunk.css"
+  },
+  {
+    "revision": "5b14b2237664bb5484dd83e3499fe8bc",
+    "url": "/index.html"
+  }
+];
